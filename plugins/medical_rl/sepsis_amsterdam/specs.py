264a29c2df034babc95e64171e917691beb4997a
Richard Weiss <richard.weiss.tuwien@gmail.com>
2024-11-21 13:52:40 +0100

    make specs handling generic

--- plugins/medical_rl/sepsis_amsterdam/specs.py
+++
@@ -1,7 +0,0 @@
-from plugins.medical_rl.sepsis_amsterdam.continuous.specs import get_observation_action_spec_sepsis_amsterdam_continuous
-from plugins.medical_rl.sepsis_amsterdam.tabular.specs import get_observation_action_spec_sepsis_amsterdam_tabular
-
-get_observation_action_spec_sepsis_amsterdam = {
-    "tabular":    get_observation_action_spec_sepsis_amsterdam_tabular,
-    "continuous": get_observation_action_spec_sepsis_amsterdam_continuous,
-}
