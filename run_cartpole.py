26de9735ed6da7dd8ba228f5bc96c4ad67ce81eb
Richard Weiss <richard.weiss.tuwien@gmail.com>
2024-11-28 16:51:22 +0100

    new runs on cartpole with simple/weighted estimator and BRA tracking

--- run_cartpole.py
+++ run_cartpole.py
@@ -1,10 +1,10 @@
 # ---------------------------------------------------------------- #
 
-from tensorflow.keras.optimizers.schedules import ExponentialDecay # type: ignore
-
 from dice_rl_TU_Vienna.runners.neural_dual_dice_runner     import NeuralDualDiceRunner
 from dice_rl_TU_Vienna.runners.neural_gen_dice_runner      import NeuralGenDiceRunner
 from dice_rl_TU_Vienna.runners.neural_gradient_dice_runner import NeuralGradientDiceRunner
+
+from dice_rl_TU_Vienna.runners.aux_recorders import aux_recorder_cos_angle
 
 from plugins.gymnasium.cartpole.load import *
 
@@ -23,30 +23,11 @@ by = "episodes"
 
 # ---------------------------------------------------------------- #
 
-# episodic
+learning_rates = []
+gammas = []
 
-learning_rates = [
-    1e-3,
-#     ExponentialDecay(
-#         initial_learning_rate=0.0005,
-#         decay_steps=50_000,
-#         decay_rate=0.1,
-#         staircase=True,
-#     ),
-#     ExponentialDecay(
-#         initial_learning_rate=0.001,
-#         decay_steps=50_000,
-#         decay_rate=0.1,
-#         staircase=True,
-#     ),
-#     ExponentialDecay(
-#         initial_learning_rate=0.001,
-#         decay_steps=100_000,
-#         decay_rate=0.5,
-#         staircase=True,
-#     ),
-]
-gammas = [0.9]
+learning_rates = [ 5e-4, 5e-4, 1e-3, ]
+gammas = [0.1, 0.5, 0.9]
 for learning_rate, gamma in zip(learning_rates, gammas):
     neural_dual_dice_runner = NeuralDualDiceRunner(
         gamma=gamma,
@@ -64,52 +45,12 @@ for learning_rate, gamma in zip(learning_rates, gammas):
         target_policy=target_policy,
         save_dir=save_dir,
         by=by,
+        aux_recorder=aux_recorder_cos_angle,
+        aux_recorder_pbar=["cos_angle"],
     )
 
-learning_rates = []
-gammas = []
-for learning_rate, gamma in zip(learning_rates, gammas):
-    neural_gen_dice_runner = NeuralGenDiceRunner(
-        gamma=gamma,
-        num_steps=num_steps,
-        batch_size=batch_size,
-        seed=seed,
-        primal_hidden_dims=hidden_dims,
-        dual_hidden_dims=hidden_dims,
-        primal_learning_rate=learning_rate,
-        dual_learning_rate=learning_rate,
-        norm_learning_rate=learning_rate,
-        regularizer_primal=regularizer_mlp,
-        regularizer_dual=regularizer_mlp,
-        regularizer_norm=regularizer_norm,
-        dataset=dataset,
-        target_policy=target_policy,
-        save_dir=save_dir,
-        by=by,
-    )
-
-learning_rates = [
-    5e-5,
-    # ExponentialDecay(
-    #     initial_learning_rate=0.001,
-    #     decay_steps=50_000,
-    #     decay_rate=0.1,
-    #     staircase=True,
-    # ),
-    # ExponentialDecay(
-    #     initial_learning_rate=0.005,
-    #     decay_steps=50_000,
-    #     decay_rate=0.1,
-    #     staircase=False,
-    # ),
-    # ExponentialDecay(
-    #     initial_learning_rate=0.005,
-    #     decay_steps=100_000,
-    #     decay_rate=0.5,
-    #     staircase=True,
-    # ),
-]
-gammas = [0.9]
+learning_rates = [ 5e-4, 1e-3, 5e-4, ]
+gammas = [0.1, 0.5, 0.9]
 for learning_rate, gamma in zip(learning_rates, gammas):
     neural_gen_dice_runner = NeuralGradientDiceRunner(
         gamma=gamma,
@@ -128,6 +69,32 @@ for learning_rate, gamma in zip(learning_rates, gammas):
         target_policy=target_policy,
         save_dir=save_dir,
         by=by,
+        aux_recorder=aux_recorder_cos_angle,
+        aux_recorder_pbar=["cos_angle"],
+    )
+
+learning_rates = [ 5e-4, 1e-4, ]
+gammas = [0.1, 0.5]
+for learning_rate, gamma in zip(learning_rates, gammas):
+    neural_gen_dice_runner = NeuralGenDiceRunner(
+        gamma=gamma,
+        num_steps=num_steps,
+        batch_size=batch_size,
+        seed=seed,
+        primal_hidden_dims=hidden_dims,
+        dual_hidden_dims=hidden_dims,
+        primal_learning_rate=learning_rate,
+        dual_learning_rate=learning_rate,
+        norm_learning_rate=learning_rate,
+        regularizer_primal=regularizer_mlp,
+        regularizer_dual=regularizer_mlp,
+        regularizer_norm=regularizer_norm,
+        dataset=dataset,
+        target_policy=target_policy,
+        save_dir=save_dir,
+        by=by,
+        aux_recorder=aux_recorder_cos_angle,
+        aux_recorder_pbar=["cos_angle"],
     )
 
 # ---------------------------------------------------------------- #
