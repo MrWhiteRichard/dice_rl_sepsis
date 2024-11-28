feb25579215084975b6ed7b58468d0f83e710888
Richard Weiss <richard.weiss.tuwien@gmail.com>
2024-11-28 16:47:34 +0100

    Update policy.py
    adapt to separating `get_observation_action_spec_boyan_chain` in `tabular` and `continuous`

--- plugins/boyan_chain/policy.py
+++ plugins/boyan_chain/policy.py
@@ -4,7 +4,7 @@ import numpy as np
 import tensorflow as tf
 
 from dice_rl_TU_Vienna.policy import MyTFPolicy
-from plugins.boyan_chain.specs import get_observation_action_spec_boyan_chain
+from plugins.boyan_chain.specs import get_observation_action_spec_boyan_chain_continuous
 
 # ---------------------------------------------------------------- #
 
@@ -14,9 +14,7 @@ class TFPolicyBoyanChain(MyTFPolicy):
         self.p = p
         self.tabular_continuous = tabular_continuous
 
-        super().__init__( *get_observation_action_spec_boyan_chain(
-            self.N, self.tabular_continuous
-        ) )
+        super().__init__( *get_observation_action_spec_boyan_chain_continuous(self.N) )
 
     def _probs(self, time_step):
         try:
