7457379817c2882fccdaf5a358a0405fe311d8bc
Richard Weiss <richard.weiss.tuwien@gmail.com>
2024-11-28 16:46:18 +0100

    load file for boyan_chain continuous

--- plugins/boyan_chain/continuous/config.py
+++ plugins/boyan_chain/continuous/config.py
@@ -2,14 +2,6 @@
 
 import os
 
-from dice_rl_TU_Vienna.dataset import load_or_create_dataset_Experience
-from plugins.boyan_chain.policy import TFPolicyBoyanChain
-from plugins.boyan_chain.environment import get_env
-from plugins.boyan_chain.analytical_solver import AnalyticalSolverBoyanChain
-
-from plugins.boyan_chain.specs import get_observation_action_spec_boyan_chain
-from dice_rl_TU_Vienna.dataset import get_dataset_spec
-
 # ---------------------------------------------------------------- #
 
 data_dir = os.path.join("data", "boyan_chain")
@@ -51,38 +43,6 @@ dataset_dir = {
             for k in K
     }
         for seed in seeds
-}
-
-# ---------------------------------------------------------------- #
-
-observation_spec, action_spec = get_observation_action_spec_boyan_chain(N, tabular_continuous)
-dataset_spec = get_dataset_spec(observation_spec, action_spec, step_num_max=2)
-
-env = {
-    seed: {
-        k: get_env(seed=seed, N=N, kind=kind[k])
-            for k in K
-    }
-        for seed in seeds
-}
-
-dataset = {
-    seed: {
-        k: load_or_create_dataset_Experience(
-            dataset_dir=dataset_dir[seed][k],
-            env=env[seed][k], num_experience=num_experience, seed=seed,
-        )
-            for k in K
-    }
-        for seed in seeds
-}
-
-target_policy = TFPolicyBoyanChain(
-    N=N, p=p, tabular_continuous=tabular_continuous)
-
-analytical_solver = {
-    k: AnalyticalSolverBoyanChain(N=N, p=p, kind=kind[k])
-        for k in K
 }
 
 # ---------------------------------------------------------------- #
