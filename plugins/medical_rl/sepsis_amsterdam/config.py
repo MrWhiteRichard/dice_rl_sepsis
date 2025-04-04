1be18f6b26141d7d9a918540fb486fe993c3597c
Richard Weiss <richard.weiss.tuwien@gmail.com>
2025-04-04 16:22:45 +0200

    Update `sepsis_amsterdam_tabular`

--- plugins/medical_rl/sepsis_amsterdam/config.py
+++ plugins/medical_rl/sepsis_amsterdam/config.py
@@ -5,6 +5,7 @@ from dice_rl_TU_Vienna.utils.os import os_path_join
 # ---------------------------------------------------------------- #
 
 names = ["original", "exploratory", "evaluation"]
+labels = ["train", "test"]
 
 # ---------------------------------------------------------------- #
 
@@ -12,20 +13,8 @@ id_split = "2025-01-30T11:42:45.694984"
 id_clustering = "2025-01-30T12:01:58.273325"
 id_policy = {
     "continuous": "2025-01-30T11:50:56.552678",
-    "tabular": {
-        "original": None,
-        "exploratory": "2025-01-31T10:25:32.802016",
-        "evaluation":  "2025-01-31T10:27:51.724072",
-    }
 }
-id_dataset = {
-    "continuous": None,
-    "tabular": {
-        "original": "2025-04-03T15:18:19.469979",
-        "exploratory": "2025-04-03T15:19:00.733248",
-        "evaluation": "2025-04-03T15:19:52.759990",
-    },
-}
+id_dataset = {}
 
 # ---------------------------------------------------------------- #
 
@@ -36,19 +25,7 @@ dir_split = os_path_join(dir_data, id_split)
 dir_clustering = os_path_join(dir_split, id_clustering)
 dir_policy = {
     "continuous": os_path_join(dir_split, id_policy["continuous"]),
-    "tabular": {
-        name: os_path_join(dir_clustering, id_policy["tabular"][name])
-            for name in names
-    }
 }
-dir_dataset = {
-    "continuous": dir_policy["continuous"],
-    "tabular": {
-        name: os_path_join(
-            dir_policy["tabular"][name], id_dataset[name],
-        )
-            for name in names
-    },
-}
+dir_dataset = {}
 
 # ---------------------------------------------------------------- #
